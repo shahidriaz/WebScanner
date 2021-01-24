138194726f6b9dcb0b324587f4ee7eac624992f4
Shahid Riaz Bhatti <shahid.bhatti@hotmail.com>
2021-01-25 00:56:14 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Async_Practical
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ColumnHeader url;
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lstScaningResult = new System.Windows.Forms.ListView();
+            this.status = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            url = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(488, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Process";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter URL";
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(105, 5);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(683, 20);
+            this.txtURL.TabIndex = 2;
+            this.txtURL.Text = "http://www.argumentexception.com";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(641, 31);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Process Async";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // lstScaningResult
+            // 
+            this.lstScaningResult.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            url,
+            this.status});
+            this.lstScaningResult.GridLines = true;
+            this.lstScaningResult.HideSelection = false;
+            this.lstScaningResult.Location = new System.Drawing.Point(37, 80);
+            this.lstScaningResult.Name = "lstScaningResult";
+            this.lstScaningResult.Size = new System.Drawing.Size(751, 338);
+            this.lstScaningResult.TabIndex = 4;
+            this.lstScaningResult.UseCompatibleStateImageBehavior = false;
+            this.lstScaningResult.View = System.Windows.Forms.View.Details;
+            // 
+            // url
+            // 
+            url.Text = "URL";
+            url.Width = 546;
+            // 
+            // status
+            // 
+            this.status.Text = "Status";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstScaningResult);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Web Scanner - By Shahid Riaz Bhatti";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView lstScaningResult;
+        private System.Windows.Forms.ColumnHeader status;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/// <summary>
+/// Code written by Shahid Riaz Bhatti on January 25, 2021
+/// This code was written to clear the concept of sync vs async programming model.
+/// Post is also written against it and its URL is http://argumentexception.com/2021/01/18/asynchronous-in-c/
+/// </summary>
+namespace Async_Practical
+{
+    public partial class Form1 : Form
+    {
+        #region Global Variable
+        List<string> listofURLS = new List<string>();
+        #endregion
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+        /// <summary>
+        /// Accept a URL and create a list and append some keywords in URL
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        private List<string> PopulateTargetURL(string url)
+        {
+            
+            listofURLS.Add(url + "/index");
+            listofURLS.Add(url + "/images");
+            listofURLS.Add(url + "/download");
+            listofURLS.Add(url + "/2006");
+            listofURLS.Add(url + "/news");
+            listofURLS.Add(url + "/crack");
+            listofURLS.Add(url + "/serial");
+            listofURLS.Add(url + "/wp-login.php");
+            return listofURLS;
+        }
+        #region Sync Call
+        /// <summary>
+        /// This method iterates through the Listof Urls and hit those urls and display 
+        /// response and their status code inside the list view
+        /// </summary>
+        private void CheckUrls()
+        {
+            foreach (var item in listofURLS)
+            {
+                if (!string.IsNullOrEmpty(item))
+                {
+                    lstScaningResult.Items.Add(new ListViewItem(new string[]
+                    {
+                    item,
+                    Convert.ToString(HitURL(item))
+                    }));
+                }
+            }
+        }
+        /// <summary>
+        /// Method which hit the URL and send back the status code
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        private HttpStatusCode HitURL(string path)
+        {
+            HttpStatusCode myHttpWebResponse = new HttpStatusCode();
+            Uri myUri = new Uri(path);
+            WebRequest request = WebRequest.Create(myUri);
+            try
+            {
+                myHttpWebResponse = ((HttpWebResponse)request.GetResponse()).StatusCode;
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.Contains("404"))
+                {
+                    return HttpStatusCode.NotFound;
+                }
+            }
+            return myHttpWebResponse;
+        }
+        #endregion
+        /// <summary>
+        /// Event for sync method
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            lstScaningResult.Items.Clear();
+            PopulateTargetURL(txtURL.Text);
+            CheckUrls();
+        }
+        /// <summary>
+        /// event for async button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            PopulateTargetURL(txtURL.Text);
+            CheckUrlsAsync();
+        }
+        /// <summary>
+        /// Iterates through the URLS in async manner and wait for the result and then display the result in list view
+        /// </summary>
+        private async void CheckUrlsAsync()
+        {
+            lstScaningResult.Items.Clear();
+            foreach (var item in listofURLS)
+            {
+                if (!string.IsNullOrEmpty(item))
+                {
+                    HttpStatusCode task = await Task.Run(()=>HitURLAsync(item));
+                    lstScaningResult.Items.Add(new ListViewItem(new string[]
+                    {
+                        item,
+                        Convert.ToString(task)
+                    }));
+                }
+            }
+        }
+        /// <summary>
+        /// Hit the URL in asycn manner
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        private async Task<HttpStatusCode> HitURLAsync(string path)
+        {
+
+            return await Task.Run(() => { 
+                HttpStatusCode myHttpWebResponse = new HttpStatusCode();
+                Uri myUri = new Uri(path);
+                WebRequest request = WebRequest.Create(myUri);
+                try
+                {
+                    myHttpWebResponse = ((HttpWebResponse)request.GetResponse()).StatusCode;
+                }
+                catch (Exception ex)
+                {
+                    if (ex.Message.Contains("404"))
+                    {
+                        return HttpStatusCode.NotFound;
+                    }
+                }
+                return myHttpWebResponse;
+            });
+            
+        }
+    }
+}
